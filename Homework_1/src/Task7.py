2899a6caba3d305dc0c90591029b958ff65302b5
socayooot <ngalay2@uccs.edu>
2026-09-24 23:02:27 +0000

    task7

--- Homework_1/src/Task7.py
+++ Homework_1/src/Task7.py
@@ -4,5 +4,9 @@ This task demonstrates the use of numpy
 import numpy as np
 
 
-def sort_array():
-    my_arr = np.array([2,4,5,6,78,0,1])
+def sort_array(arr):
+    return np.sort(arr)
+
+
+def find_min_max(arr):
+    return f"here is the min: {np.min(arr)}, here is the max: {np.max(arr)}"
--- Homework_1/tests/test_task7.py
+++ Homework_1/tests/test_task7.py
@@ -4,9 +4,20 @@ import numpy as np
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
-from Task7 import sort_array
+from Task7 import sort_array, find_min_max
 
+#arrays
 arr = np.array([2,4,5,6,7,1,0])
+arr2 = np.array([989,12,3,6543234,76,9])
 
-new_arr = sort_array(arr)
-print(new_arr)
+#testing sort
+sort_arr = sort_array(arr)
+print(*sort_arr)
+sort_arr2 = sort_array(arr2)
+print(*sort_arr2)
+
+print()
+
+#testing max
+min_arr = find_min_max(arr)
+print(min_arr)
